2e591476ffcddd08c9002dd4d6bd14680ffac26c
LarcesUece <larces@uece.br>
2022-12-07 14:28:15 -0300

    feat: add initial prototype

---
+++ config.py
@@ -0,0 +1,14 @@
+# Informations Api
+BASE_URL = "http://127.0.0.1:5000"
+
+# Informations client
+USER_EMAIL = ""
+USER_PASSWORD = ""
+USER_ID = None
+
+# Informations client database
+ID_DB = None
+SRC_CLIENT_DB_PATH = "
+
+# Informations user database
+SRC_USER_DB_PATH = ""
